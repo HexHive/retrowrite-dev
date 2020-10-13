e7b9b5b84e4acda8a0c45d50080247e1efbd914a
cyanpencil <lucadb96@gmail.com>
2020-10-13 14:26:56 +0200

    Experiment: no literal pools 3 iterations

--- arm/librw/analysis/register.py
+++ arm/librw/analysis/register.py
@@ -233,5 +233,5 @@ class RegisterAnalysis(object):
             #XXX
             #XXX
             #XXX
-            self.free_regs[idx] = []
-            # self.free_regs[idx] = self.reg_pool.difference(ent)
+            # self.free_regs[idx] = []
+            self.free_regs[idx] = self.reg_pool.difference(ent)
