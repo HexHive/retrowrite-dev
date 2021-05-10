0b9db3e819658869f1e732ab43cfea4a550e6b9f
Luca Di Bartolomeo <lucadb96@gmail.com>
2021-05-10 20:22:31 +0200

    Experiment:  measure with perf, only symbolization

--- retrowrite_ci/run_test.py
+++ retrowrite_ci/run_test.py
@@ -87,7 +87,8 @@ print("="*50)
 print("Finished. You can now run:")
 # print(f"cd {PATH_SPECPU} && source shrc && runcpu --nobuild --iterations 1 --config strace.cfg {final_str}")
 # print(f"cd {PATH_SPECPU} && source shrc && runcpu --nobuild --iterations 1 --config final.cfg {final_str}")
-print(f"cd {PATH_SPECPU} && source shrc && runcpu --rebuild --iterations 1 --config final_ASAN.cfg {final_str}")
+# print(f"cd {PATH_SPECPU} && source shrc && runcpu --rebuild --iterations 1 --config final_ASAN.cfg {final_str}")
 # print(f"cd {PATH_SPECPU} && source shrc && runcpu --rebuild --iterations 1 --config valgrind.cfg {final_str}")
+print(f"cd {PATH_SPECPU} && source shrc && runcpu --rebuild --iterations 1 --config perf.cfg {final_str}")
 # --size test --loose --fake 
 
