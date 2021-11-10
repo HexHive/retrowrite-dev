7c7d96027e5b7c53db937e3ab85103158ce2c720
Hossein Moghaddas <autquis@gmail.com>
2021-11-10 17:03:31 +0100

    Not tested!

--- arm/rwtools/simple_cfi/instrument.py
+++ arm/rwtools/simple_cfi/instrument.py
@@ -21,11 +21,11 @@ class Instrument():
         add x8, x8, :lo12:.value
         ldr x8, [x8]
         cmp x7, x8
-        b.eq 8                        // jump over crash
-        .word 0x0                     // crash
-        ldp x7, x8, [sp], 16          // restore regs and continue
+        b.eq 8                          // jump over crash
+        brk 1                           // crash
+        ldp x7, x8, [sp], 16            // restore regs and continue
         // END CFI CHECK
-        add {jump_reg}, {jump_reg}, 8 // skip first 8 bytes of known value
+        add {jump_reg}, {jump_reg}, 8   // skip first 8 bytes of known value
         """
 
         return InstrumentedInstruction(instrumentation, enter_lbl, comment)
@@ -33,6 +33,16 @@ class Instrument():
 
 
     def do_instrument(self):
+        for _, fn in self.rewriter.container.functions.items():
+            if fn.name in ["_start", "close_stdout"] or len(fn.cache) == 0:
+                continue
+            first_instr = fn.cache[0]
+            first_instr.before.insert(0, InstrumentedInstruction("paciasp"))
+            #NOTE Assumption: ret is always the last instruction.
+            last_instr = fn.cache[-1]
+            last_instr.before.insert(0, InstrumentedInstruction("autiasp"))
+
+        #NOTE First check for the 
         for _, fn in self.rewriter.container.functions.items():
             if fn.name in ["_start", "main", "close_stdout"] or len(fn.cache) == 0:
                 continue
